7fa299fb10c886ce9964d607c536186cb6c0fa78
Samuel Centeno <ttiger6666@gmail.com>
2023-06-15 23:48:32 -0300

    updates
    general update

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/itcrowd`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/postgres`,
         { logging: false, native: false }
       );
 
